6b5a742a0c777750bf7d6cadad06e4adfba6c4d8
Mike Gleen <mike.gleen@gmail.com>
2020-10-29 08:08:27 +0000

    Convert Node.goal from boolean to integer count of adjacent cells with wells.

--- src/giganten.py
+++ src/giganten.py
@@ -22,7 +22,7 @@ class Node:
     """
     cost: Number of movement points it costs to enter this node.
     exhausted: True if a derrick has been built and all of the oil has been extracted.
-    goal: True if a neighbor is an oil well; changes to False if a derrick is built
+    goal: Number of adjacent cells with wells. Decremented if a derrick is built
     """
 
     def set_neighbors(self, board):
@@ -47,8 +47,8 @@ class Node:
             # If the neighbor has wells, you aren't allowed to stop there,
             # so it can't be a goal.
             if neighbor.wells == 0:
-                neighbor.goal |= self.wells > 0 and not (self.derrick or
-                                                         self.exhausted)
+                if self.wells and not (self.derrick or self.exhausted):
+                    neighbor.goal += 1
 
         lastrow = len(board) - 1
         lastcol = len(board[0]) - 1
@@ -66,10 +66,14 @@ class Node:
         # Make this node impassable
         assert self.derrick is False
         self.derrick = True
+        for node in self.adjacent:
+            assert node.goal > 1  # assume no adjacent cells with wells
+            node.goal -= 1
 
     def remove_derrick(self):
         # Make this node passable
-        pass
+        assert self.derrick is True
+        self.derrick = False
 
     def print_path(self):
         nextprev = self.previous
@@ -81,23 +85,24 @@ class Node:
         path.reverse()
         print('   ', path)
 
-    def __init__(self, row, col):
+    def __init__(self, row, col, derrick=False):
         self.row: int = row
         self.col: int = col
         self.id: str = f'[{row},{col}]'
         self.terrain: int = 0
         self.wells: int = 0
         self.exhausted: bool = False
-        self.goal: bool = False
-        self.derrick: bool = False
+        self.goal: int = 0  # count of adjacent nodes with unbuilt wells
+        self.derrick: bool = derrick
         self.barrels = 0
         self.adjacent = []  # will be populated by set_neighbors
         self.distance: int = sys.maxsize
         self.previous = None  # will be set when visited
+        self.cell = None  # this node's string from rawboard
 
     def __str__(self):
         e = 'T' if self.exhausted else 'F'
-        g = 'T' if self.goal else 'F'
+        g = self.goal
         d = 'T' if self.derrick else 'F'
         # s = f'node[{self.row},{self.col}] terrain: {self.terrain}, '
         # s += f'wells: {self.wells} '
@@ -142,7 +147,9 @@ class Graph:
         for r, row in enumerate(board):
             for c, node in enumerate(row):
                 # See read_board() for a description of the pattern
-                m = re.match(r'(\d?)(\.(\d)(x?))?', cell := rawboard[r][c])
+                m = re.match(r'(\d?)(\.(\d?)([xd]?))?', cell := rawboard[r][c])
+                node.cell = cell
+                # print(f'{r=} {c=} {m.group(1,2,3,4)=}')
                 if m is None:
                     raise ValueError(
                         f"In row {r}, col {c}, '{cell}' failed match.")
@@ -150,6 +157,11 @@ class Graph:
                 if m.group(3):  # if wells are specified
                     node.wells = 0 if (m.group(4) == 'x' and three_players
                                        ) else int(m.group(3))
+                # For testing, force a derrick
+                # print(node)
+                # print(f'{m.group(4)=}')
+                if m.group(4) == 'd':
+                    node.derrick = True
         self.board = board
         self.rows = nrows
         self.columns = ncols
@@ -178,6 +190,24 @@ class Graph:
                     print('    printing path: ', end='')
                     node.print_path()
 
+    def dump_raw_board(self):
+        """
+            This is a little utility method to create a file readable as an
+            input board CSV file. This is useful if the original CSV file was
+            created manually by columns but we want to proceed with the
+            file as rows as this is more understandable.
+        """
+        outfilename = _args.dumprawboard
+        outfile = open(outfilename, 'w')
+        for row in range(self.rows):
+            r = ''
+            for col in range(self.columns):
+                node = self.board[row][col]
+                cell = node.cell
+                # r += ' ' * (5 - len(cell)) + cell
+                r += f'{cell:>5}'
+            print(r, file=outfile)
+
     def print_board_narrow(self):
 
         def pr_dist(node):
@@ -194,7 +224,7 @@ class Graph:
             r1 = [Graph.TERRAIN_CH[n.terrain] + pr_dist(n) for n in row]
             print(f' {nrow:02}|' + '|'.join(r1) + '|')
             r2 = [(pr_wells(n) * n.wells) + (' ' * (3 - n.wells))
-                  + ('G' if n.goal else ' ') for n in row]
+                  + (n.goal if n.goal else ' ') for n in row]
             print('   |' + '|'.join(r2) + '|')
         print('   ' + '|————' * self.columns + '|')
 
@@ -205,7 +235,7 @@ class Graph:
             return f'{dist:2d}' if dist < sys.maxsize else '  '
 
         def pr_wells(node):
-            return'D' if node.derrick else 'W'
+            return 'D' if node.derrick else 'w'
 
         def from_arrow(node):
             if not (previous := node.previous):
@@ -217,7 +247,6 @@ class Graph:
                 return (UPWARDS_ARROW if node.row > previous.row else
                         DOWNWARDS_ARROW)
 
-        # self.board[4][13].derrick = True  # test feature
         print('   ' + ''.join([f'| {n:03d} ' for n in range(self.columns)]) + '|')
         for nrow, row in enumerate(self.board):
             print('   ' + '|—————' * self.columns + '|')
@@ -225,24 +254,27 @@ class Graph:
             print(f' {nrow:02}|' + '|'.join(r1) + '|')
             r2 = [(pr_wells(n) * n.wells) + (' ' * (3 - n.wells))
                   + from_arrow(n)
-                  + (Graph.RED + 'G' + Graph.RESET if n.goal else ' ') for n in row]
+                  + (Graph.RED + str(n.goal) + Graph.RESET if n.goal else ' ')
+                  for n in row]
             print('   |' + '|'.join(r2) + '|')
         print('   ' + '|—————' * self.columns + '|')
 
 
 def read_board(csvfile):
     """
-    Each line in the csv file describes one column of the board. (It was easier
-    to type this way). Each line contains cells separated by white space.
+    Each line in the csv file describes one row or one column of the board,
+    depending on argument --bycols.
     Each cell contains:
 
         <cell> ::= <terrain> | <terrain> "." <oilwell> | "." <oilwell>
         <terrain> ::= 1 | 2 | 3
-        <oilwell> ::= <wellcount> | <wellcount> <3-player marker>
+        <oilwell> ::= <wellcount> | <wellcount> <marker>
         <wellcount> ::= 1 | 2 | 3
-        <3-player marker> = "x"
+        <marker> = "x" | "d"
 
     If <terrain> is absent, "1" is assumed.
+    If <marker> == "x" then this is a cell ignored for 3-person games.
+    If <marker> == "d" then this cell contains a derrick (used for testing).
 
     :param csvfile: The file containing the board as described above.
     :return: A list of rows containing tuples corresponding to the columns.
@@ -259,19 +291,22 @@ def read_board(csvfile):
             raise ValueError(f"Length of line {nline + 1} is "
                              f"{len(c)}, {nrows} expected.")
         r.append(c)
-    # board = list(map(list, zip(*board)))  # create list of lists, not tuples
-    # Invert the array giving a list of tuples where each tuple is one row.
-    rawboard = list(zip(*r))
+    if _args.bycols:
+        # Invert the array giving a list of tuples where each tuple is one row.
+        rawboard = list(zip(*r))
+    else:
+        rawboard = r
     if _args.verbose >= 2:
         print('rawboard:', rawboard)
     return rawboard
 
 
-def dijkstra(root: Node, maxcost=sys.maxsize):
+def dijkstra(root: Node, maxcost=sys.maxsize, verbose=1):
     """
     :param root: the Node to start from. The Nodes have been initialized
              during the creation of the Graph instance.
     :param maxcost: Do not search for nodes more than maxcost away.
+    :param verbose: the debug level
     :return: The Node's distance field is updated in each instance in the
              board that is reachable from the root node given the game's
              constraints such as within the maximum distance from the root
@@ -285,19 +320,21 @@ def dijkstra(root: Node, maxcost=sys.maxsize):
         # Pops a vertex with the smallest distance
         current = heapq.heappop(unvisited_queue)
         visited.add(current)
-        if _args.verbose >= 3:
-            print(f'{current=}')
+        if verbose >= 3:
+            print(f'{current=} {current.adjacent=}')
         if current.distance >= maxcost:
-            if _args.verbose >= 3:
+            if verbose >= 3:
                 print(f'    stopping at distance {current.distance}.')
             continue
         for nextn in sorted(current.adjacent):  # iterate over adjacent nodes
+            if verbose >= 3:
+                print(f'{nextn=} {"DERRICK" if nextn.derrick else ""}')
             if nextn in visited:  # if visited, skip
-                if _args.verbose >= 3:
+                if verbose >= 3:
                     print(f'skipping, visited: {nextn=}')
                 continue
             if nextn.derrick:
-                if _args.verbose >= 3:
+                if verbose >= 3:
                     print(f'skipping, derrick: {nextn=}')
                 continue
             new_dist = current.distance + nextn.terrain
@@ -306,7 +343,7 @@ def dijkstra(root: Node, maxcost=sys.maxsize):
             # If the next node has wells and is not exhausted, the player
             # may not stop there.
             if nextn.wells and not nextn.exhausted and new_dist >= maxcost:
-                if _args.verbose >= 3:
+                if verbose >= 3:
                     print(f'skipping, wells: {nextn=}')
                 continue
             if new_dist < nextn_dist and new_dist <= maxcost:
@@ -316,18 +353,20 @@ def dijkstra(root: Node, maxcost=sys.maxsize):
                 updated = 'updated'  # just used for logging
             else:
                 updated = 'not updated'
-            if _args.verbose >= 3:
+            if verbose >= 3:
                 # print('%s : current = %s next = %s new_dist = %s'
                 #       % (updated, current.id, nextn.id, nextn_dist))
                 print(f'{updated}: current: {current.id}, next: {nextn.id}, '
                       f'dist: {idist(nextn_dist)} -> {nextn.distance}')
-        if _args.verbose >= 3:
+        if verbose >= 3:
             print(f'unvisited: {unvisited_queue}')
 
 
 def main(args):
     rawboard = read_board(args.incsv)
     graph = Graph(rawboard, args.nplayers)
+    if args.dumprawboard:
+        graph.dump_raw_board()
     if args.verbose >= 3:
         graph.dump_board()
     nrows, ncols = graph.get_rows_cols()
@@ -340,13 +379,13 @@ def main(args):
         for _ in range(args.timeit):
             graph.reset_distance()
             dijkstra(graph.board[args.row][args.column],
-                     maxcost=args.maxcost)
+                     maxcost=args.maxcost, verbose=_args.verbose)
         t2 = time.time()
         print(t2 - t1)
     else:
         graph.reset_distance()
         dijkstra(graph.board[args.row][args.column],
-                 maxcost=args.maxcost)
+                 maxcost=args.maxcost, verbose=_args.verbose)
     if args.verbose >= 2:
         graph.dump_board()
     # print(board)
@@ -361,8 +400,14 @@ def getargs():
         ''')
     parser.add_argument('incsv', type=argparse.FileType('r'), help='''
          The file containing the board description.''')
+    parser.add_argument('--bycols', action='store_true', help='''
+    The input CSV file contains data by columns and needs to be flipped.
+    ''')
     parser.add_argument('-c', '--column', type=int, default=0, help='''
     Start column.
+    ''')
+    parser.add_argument('--dumprawboard', help='''
+    Specify the file to dump the raw board to.
     ''')
     parser.add_argument('-m', '--maxcost', type=int, default=sys.maxsize,
                         help='''
